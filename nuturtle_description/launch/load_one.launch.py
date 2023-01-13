02dfe436597f97fa74bc31425fc7f4661d34a40a
Kevin Nella <kevin.nella@northwestern.edu>
2023-01-12 19:15:38 -0600

    created launchfile

--- nuturtle_description/launch/load_one.launch.py
+++ nuturtle_description/launch/load_one.launch.py
@@ -1,11 +1,36 @@
-import launch
-import launch_ros.actions
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare, ExecutableInPackage
 
 def generate_launch_description():
-    return launch.LaunchDescription([
-        launch_ros.actions.Node(
-            package='demo_nodes_cpp',
-            executable='talker',
-            name='talker',
-        )
+        
+    return LaunchDescription([
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher'
+            ),
+    
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            parameters=[
+                {'robot_description':
+                Command([ExecutableInPackage('xacro','xacro'), ' ',
+                        PathJoinSubstitution(
+                        [FindPackageShare('nuturtle_description'), 'turtlebot3_burger.urdf.xacro'])
+                        ])}
+                        ]
+            ),
+
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen'
+            )
     ])
