d1acdbc36a23ffd2ce4421ef10d5f71963fc251a
Kevin Nella <kevin.nella@northwestern.edu>
2023-01-15 11:51:13 -0600

    finally got >> operator working

--- turtlelib/rigid2d.cpp
+++ turtlelib/rigid2d.cpp
@@ -1,18 +1,44 @@
 #include "rigid2d.hpp"
 #include <iostream>
+#include <string>
 
+// std::ostream & turtlelib::operator<<(std::ostream & os, const Vector2D & v)
+// {
+//     os << '[' << v.x << ' ' << v.y << ']' << '\n';
+//     return os;
+// }
 
-std::ostream & turtlelib::operator<<(std::ostream & os, const Vector2D & v)
+std::istream & turtlelib::operator>>(std::istream & is, Vector2D & v)
 {
-    os << '[' << v.x << ' ' << v.y << ']' << '\n';
-    return os;
+    int first{0};
+    char x{};
+    while (std::cin.peek()!='\n')
+    {
+        x = std::cin.peek();
+        if (x == ' ')
+        {
+            first = 1;
+            std::cin.get(x);
+        }
+        else if (std::isdigit(x)||x == '-')
+        {   
+            (first==0)?is>>v.x:is>>v.y;
+        }
+        else
+        {
+            std::cin.get(x);
+        }
+    }
+    return is;
 }
-
 
 
 int main()
 {
-    const turtlelib::Vector2D v{1,2};
-    std::cout << v;
+    turtlelib::Vector2D k{};
+    std::cout<<"Enter 2D vector separated by space\n";
+    std::cin>>k;
+    std::cout<<k.x<<' '<<k.y<<'\n';
+
     return 0;
 }
