cf15570c24e3869ba22c88abe554b7fdafcb270b
Kevin Nella <kevin.nella@northwestern.edu>
2023-01-20 09:36:46 -0600

    fixed problems with cmakelists. implemented primitive reset service in SimNode

--- nusim/src/nusim.cpp
+++ nusim/src/nusim.cpp
@@ -4,14 +4,15 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/u_int64.hpp"
+#include "nusim/srv/reset.hpp"
 
 
 using namespace std::chrono_literals;
 
-class MinimalPublisher : public rclcpp::Node
+class SimNode : public rclcpp::Node
 {
   public:
-    MinimalPublisher()
+    SimNode()
     : Node("nusim"), count_(0)
     {
     
@@ -23,27 +24,44 @@ class MinimalPublisher : public rclcpp::Node
 
     publisher_ = this->create_publisher<std_msgs::msg::UInt64>("~/timestep", 10);
     timer_ = this->create_wall_timer(
-    std::chrono::duration<int64_t,std::milli>(t), std::bind(&MinimalPublisher::timer_callback, this));
+    std::chrono::duration<int64_t,std::milli>(t), std::bind(&SimNode::timer_callback, this));
+
+    service_ = this->create_service<nusim::srv::Reset>("reset", std::bind(&SimNode::reset, this, std::placeholders::_1, std::placeholders::_2));
+
     }
 
   private:
+
     void timer_callback()
     {
-      auto message = std_msgs::msg::UInt64();
-      count_++;
-      message.data = count_;
-      RCLCPP_INFO(this->get_logger(), "Publishing: %lu", message.data);
-      publisher_->publish(message);
+    auto message = std_msgs::msg::UInt64();
+    count_++;
+    message.data = count_;
+    RCLCPP_INFO(this->get_logger(), "Publishing: %lu", message.data);
+    publisher_->publish(message);
     }
+
+    void reset(const std::shared_ptr<nusim::srv::Reset::Request> request,
+               std::shared_ptr<nusim::srv::Reset::Response> response)
+    {
+        count_ = 0;
+        (void)request;
+        (void)response;
+        RCLCPP_INFO(this->get_logger(), "Incoming request to reset");
+        RCLCPP_INFO(this->get_logger(), "Resetting");
+    }
+    
+    
     rclcpp::TimerBase::SharedPtr timer_;
     rclcpp::Publisher<std_msgs::msg::UInt64>::SharedPtr publisher_;
+    rclcpp::Service<nusim::srv::Reset>::SharedPtr service_;
     size_t count_;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::spin(std::make_shared<SimNode>());
   rclcpp::shutdown();
   return 0;
 }
