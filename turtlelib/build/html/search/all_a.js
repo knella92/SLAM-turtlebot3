6b77a3378e73b69b3d912f4062ea29a2c7a34ee8
Kevin Nella <kevin.nella@northwestern.edu>
2023-01-22 20:50:53 -0600

    updating main branch

---
+++ turtlelib/build/html/search/all_a.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['x_20',['x',['../structturtlelib_1_1Vector2D.html#a8e675b8f19a35056218cbb41f7cb4341',1,'turtlelib::Vector2D']]]
+];
---
+++ turtlelib/build/html/search/functions_6.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['transform2d_37',['Transform2D',['../classturtlelib_1_1Transform2D.html#af02e23ce05599c9b378ea6bfbcc8d2dc',1,'turtlelib::Transform2D::Transform2D()'],['../classturtlelib_1_1Transform2D.html#a4ddd1cc18c672d413ebfd6752d33d554',1,'turtlelib::Transform2D::Transform2D(Vector2D trans)'],['../classturtlelib_1_1Transform2D.html#a206dfc425001c3a0ca1ab5336690f630',1,'turtlelib::Transform2D::Transform2D(double radians)'],['../classturtlelib_1_1Transform2D.html#aeaf27ff4e5703a7a464f6f6fdb81d23f',1,'turtlelib::Transform2D::Transform2D(Vector2D trans, double rot)']]],
+  ['translation_38',['translation',['../classturtlelib_1_1Transform2D.html#aa04233cce5d1dd03fc28f1372bc8ae6d',1,'turtlelib::Transform2D']]],
+  ['twist2d_39',['Twist2D',['../structturtlelib_1_1Twist2D.html#a97a7053f141781c9e9d706ef2ecff6bd',1,'turtlelib::Twist2D::Twist2D()'],['../structturtlelib_1_1Twist2D.html#a49b06a8cbb8e8d1ae5d66bebfc4be3e4',1,'turtlelib::Twist2D::Twist2D(double w, Vector2D v)']]]
+];
