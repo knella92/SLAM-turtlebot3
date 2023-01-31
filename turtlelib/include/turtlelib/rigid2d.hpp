a9e29f44357db251591fabc379ba319b6fbd281d
Kevin Nella <kevin.nella@northwestern.edu>
2023-01-30 19:42:01 -0600

    commit before push

---
+++ turtlelib/include/turtlelib/rigid2d.hpp
@@ -0,0 +1,296 @@
+#ifndef RIGID2D_INCLUDE_GUARD_HPP
+#define RIGID2D_INCLUDE_GUARD_HPP
+/// \file
+/// \brief Two-dimensional rigid body transformations.
+
+
+#include<iosfwd> // contains forward definitions for iostream objects
+#include<iostream>
+#include <cmath>
+#include <vector>
+
+namespace turtlelib
+{
+    /// \brief PI.  Not in C++ standard until C++20.
+    constexpr double PI=3.14159265358979323846;
+
+    /// \brief approximately compare two floating-point numbers using
+    ///        an absolute comparison
+    /// \param d1 - a number to compare
+    /// \param d2 - a second number to compare
+    /// \param epsilon - absolute threshold required for equality
+    /// \return true if abs(d1 - d2) < epsilon
+    /// NOTE: implement this in the header file
+    /// constexpr means that the function can be computed at compile time
+    /// if given a compile-time constant as input
+    constexpr bool almost_equal(double d1, double d2, double epsilon=1.0e-12)
+    {
+        return (abs(d1-d2)<epsilon)?true:false;
+    }
+
+    /// \brief convert degrees to radians
+    /// \param deg - angle in degrees
+    /// \returns radians
+    constexpr double deg2rad(double deg)
+    {
+        return (deg/360.0*2.0*PI);
+    }
+
+    /// \brief convert radians to degrees
+    /// \param rad - angle in radians
+    /// \returns the angle in degrees
+    constexpr double rad2deg(double rad)
+    {
+        return (rad/(2.0*PI)*360.0);
+    }
+
+    /// static_assertions test compile time assumptions.
+    /// You should write at least one more test for each function
+    /// You should also purposely (and temporarily) make one of these tests fail
+    /// just to see what happens
+    static_assert(almost_equal(0, 0), "is_zero failed");
+    static_assert(almost_equal(3.0+1e-13,3.0), "is_zero failed");
+
+    static_assert(almost_equal(deg2rad(0.0), 0.0), "deg2rad failed");
+    static_assert(almost_equal(deg2rad(360), 2*PI), "deg2rad failed");
+
+    static_assert(almost_equal(rad2deg(0.0), 0.0), "rad2deg) failed");
+    static_assert(almost_equal(rad2deg(2*PI), 360.0), "rad2deg failed");
+
+    static_assert(almost_equal(deg2rad(rad2deg(2.1)), 2.1), "deg2rad failed");
+    static_assert(almost_equal(deg2rad(rad2deg(45)), 45), "deg2rad failed");
+
+    /// \brief A 2-Dimensional Vector
+    struct Vector2D
+    {
+        /// \brief the x coordinate
+        double x = 0.0;
+
+        /// \brief the y coordinate
+        double y = 0.0;
+
+        /// \brief compute addition of two 2D vectors and store result in this object
+        /// \param rhs - vector to add to original
+        /// \return sum of the two vectors
+        Vector2D& operator+=(const Vector2D& rhs);
+
+        /// \brief compute subtraction of two 2D vectors and store result in this object
+        /// \param rhs - vector to subtract from the original
+        /// \return difference of the two vectors
+        Vector2D& operator-=(const Vector2D& rhs);
+
+    };
+
+    /// \brief compute the dot product of two 2D vectors
+    /// \param vec1 - first vector
+    /// \param vec2 - second vector
+    /// \return - dot product with double point precision
+    double dot(Vector2D& vec1, Vector2D& vec2);
+
+    /// \brief compute the magnitude of a 2D vector
+    /// \param vec - vector
+    /// \return - magnitude of the vector
+    double magnitude(Vector2D& vec);
+
+    /// \brief compute the angle between two 2D vectors
+    /// \param vec1 - first vector
+    /// \param vec2 - second vector
+    /// \return - angle between the vectors
+    double angle(Vector2D& vec1, Vector2D& vec2);
+
+    /// \brief output the normalized 2D vector of the input parameter
+    /// \param v - 2D vector input
+    /// \return normalized vector
+    Vector2D normalize(Vector2D v);
+
+    /// \brief adds two vectors together, returning their sum
+    /// \param lhs - left hand side of operator
+    /// \param rhs - right hand side of operator
+    /// \return - the vector sum
+    Vector2D operator+(Vector2D lhs, const Vector2D rhs);
+
+    /// \brief gives difference between two vectors together
+    /// \param lhs - left hand side of operator
+    /// \param rhs - right hand side of operator
+    /// \return - the vector difference
+    Vector2D operator-(Vector2D lhs, const Vector2D rhs);
+
+    /// \brief gives product of left scalar multiplication of 2D vector
+    /// \param lhs - scalar value (left side)
+    /// \param rhs - 2D vector to be multiplied (right side)
+    /// \return - the final 2D vector
+    Vector2D operator*(double lhs, const Vector2D rhs);
+
+    /// \brief gives product of right scalar multiplication of 2D vector
+    /// \param lhs - 2D vector (left side)
+    /// \param rhs - scalar value to multiply by (right side)
+    /// \return - the final 2D vector
+    Vector2D operator*(Vector2D lhs, const double rhs);
+
+
+
+    /// \brief output the normalized angle of the input paramater
+    /// \param rad - angular rotation input
+    /// \return normalized angular rotation
+    double normalize_angle(double rad);
+
+    /// \brief output a 2 dimensional vector as [xcomponent ycomponent]
+    /// os - stream to output to
+    /// v - the vector to print
+    std::ostream & operator<<(std::ostream & os, const Vector2D & v);
+
+    /// \brief input a 2 dimensional vector
+    ///   You should be able to read vectors entered as follows:
+    ///   [x y] or x y
+    /// \param is - stream from which to read
+    /// \param v [out] - output vector
+    ///
+    /// The way input works is (more or less): what the user types is stored in a buffer until the user types
+    /// a newline (by pressing enter).  The iostream methods then process the data in this buffer character by character.
+    /// Typically, each character is examined and then removed from the buffer automatically.
+    /// If the characters don't match what is expected (e.g., we are expecting an int but the letter 'q' is encountered)
+    /// an error flag is set on the stream object (e.g., std::cin) and processing stops.
+    ///
+    /// We have lower level control however.
+    /// std::peek() looks at the next unprocessed character in the buffer without removing it
+    ///     https://en.cppreference.com/w/cpp/io/basic_istream/peek
+    /// std::get() removes the next unprocessed character from the buffer.
+    ///     https://en.cppreference.com/w/cpp/io/basic_istream/get
+    /// When you call std::peek() it will wait for there to be at least one character in the buffer (e.g., the user types a character)
+    /// HINT: this function can be written in under 20 lines and uses only std::peek(), std::get(), istream::operator>>() and a little logic
+    std::istream & operator>>(std::istream & is, Vector2D & v);
+
+    /// \brief A twist in two dimensions 
+    struct Twist2D
+    {
+        /// \brief the angular velocity
+        double w;
+        
+        /// \brief the 2D vector of the translational velocity
+        Vector2D v;
+
+        /// \brief Create an empty twist
+        Twist2D();
+
+        /// \brief Create a twist with a rotational and translational
+        /// component
+        /// \param w - the angular velocity input
+        /// \param v - the 2D vector of the translational velocity input
+        Twist2D(double w, Vector2D v);
+
+        /// \brief \see operator<<(...) (declared outside this class)
+        /// for a description
+        friend std::ostream & operator<<(std::ostream & os, const Twist2D & tw);
+
+    };
+
+    /// \brief a rigid body transformation in 2 dimensions
+    class Transform2D
+    {
+    private:
+
+        /// \brief a private vector to store the transformation
+        std::vector<std::vector<double>> transf;
+
+    public:
+        /// \brief Create an identity transformation
+        Transform2D();
+
+        /// \brief create a transformation that is a pure translation
+        /// \param trans - the vector by which to translate
+        explicit Transform2D(Vector2D trans);
+
+        /// \brief create a pure rotation
+        /// \param radians - angle of the rotation, in radians
+        explicit Transform2D(double radians);
+
+        /// \brief Create a transformation with a translational and rotational
+        /// component
+        /// \param trans - the translation
+        /// \param rot - the rotation, in radians
+        Transform2D(Vector2D trans, double rot);
+
+        /// \brief apply a transformation to a Vector2D
+        /// \param v - the vector to transform
+        /// \return a vector in the new coordinate system
+        Vector2D operator()(Vector2D v) const;
+
+
+        /// \brief invert the transformation
+        /// \return the inverse transformation. 
+        Transform2D inv() const;
+
+        /// \brief compose this transform with another and store the result 
+        /// in this object
+        /// \param rhs - the first transform to apply
+        /// \return a reference to the newly transformed operator
+        Transform2D & operator*=(const Transform2D & rhs);
+
+        /// \brief the translational component of the transform
+        /// \return the x,y translation
+        Vector2D translation() const;
+
+        /// \brief get the angular displacement of the transform
+        /// \return the angular displacement, in radians
+        double rotation() const;
+
+        /// \brief \see operator<<(...) (declared outside this class)
+        /// for a description
+        friend std::ostream & operator<<(std::ostream & os, const Transform2D & tf);
+
+        /// \brief convert a twist V to a different reference frame
+        /// \param V - the twist to transform
+        /// \return a twist in the new reference frame
+        Twist2D operator()(Twist2D V) const;
+
+    };
+
+
+    /// \brief should print a human readable version of the transform:
+    /// An example output:
+    /// deg: 90 x: 3 y: 5
+    /// \param os - an output stream
+    /// \param tf - the transform to print
+    std::ostream & operator<<(std::ostream & os, const Transform2D & tf);
+
+    /// \brief Read a transformation from stdin
+    /// Should be able to read input either as output by operator<< or
+    /// as 3 numbers (degrees, dx, dy) separated by spaces or newlines
+    /// For example:
+    /// 90 2 3
+    std::istream & operator>>(std::istream & is, Transform2D & tf);
+
+    /// \brief multiply two transforms together, returning their composition
+    /// \param lhs - the left hand operand
+    /// \param rhs - the right hand operand
+    /// \return the composition of the two transforms
+    /// HINT: This function should be implemented in terms of *=
+    Transform2D operator*(Transform2D lhs, const Transform2D & rhs);
+
+    /// \brief should print a human readable version of the Twist:
+    /// An example output:
+    /// [1 3 5]
+    /// \param os - an output stream
+    /// \param tw - the twist to print
+    std::ostream & operator<<(std::ostream & os, const Twist2D & tw);
+
+    /// \brief Read a twist from stdin
+    /// Should be able to read input either as output by operator<< or
+    /// as 3 numbers (w x y) separated by spaces
+    /// For example:
+    /// 1 2 3
+    /// \param is - input stream
+    /// \param tw - input twist
+    std::istream & operator>>(std::istream & is, Twist2D & tw);
+
+    /// \brief Computes the transformation corresponding to a rigid body
+    /// following a constant twist (in its original body frame) for one
+    /// time unit
+    /// \param Vb - constant twist within original body frame of rigid body
+    /// \return transformation corresponding new body frame from original body frame
+    Transform2D integrate_twist(Twist2D& Vb);
+
+
+}
+
+#endif
---
+++ turtlelib/src/rigid2d.cpp
@@ -0,0 +1,406 @@
+#include "turtlelib/rigid2d.hpp"
+
+std::ostream & turtlelib::operator<<(std::ostream & os, const Vector2D & v)
+{
+    os << '[' << v.x << ' ' << v.y << ']';
+    return os;
+}
+
+std::istream & turtlelib::operator>>(std::istream & is, Vector2D & v)
+{
+    int first{0};
+    char x{};
+    while (is.peek()!='\n')
+    {
+        x = is.peek();
+        if (x == ' ')
+        {
+            first = 1;
+            is.get(x);
+        }
+        else if (std::isdigit(x)||x == '-')
+        {   
+            (first==0)?is>>v.x:is>>v.y;
+        }
+        else
+        {
+            is.get(x);
+        }
+    }
+    return is;
+}
+
+turtlelib::Vector2D& turtlelib::Vector2D::operator+=(const turtlelib::Vector2D& rhs)
+{
+    this->x = this->x + rhs.x;
+    this->y = this->y + rhs.y;
+
+    return *this;
+}
+
+turtlelib::Vector2D turtlelib::operator+(turtlelib::Vector2D lhs, const turtlelib::Vector2D rhs)
+{
+    lhs+=rhs;
+    return lhs;
+}
+
+turtlelib::Vector2D& turtlelib::Vector2D::operator-=(const turtlelib::Vector2D& rhs)
+{
+    this->x = this->x - rhs.x;
+    this->y = this->y - rhs.y;
+
+    return *this;
+}
+
+turtlelib::Vector2D turtlelib::operator-(turtlelib::Vector2D lhs, const turtlelib::Vector2D rhs)
+{
+    lhs-=rhs;
+    return lhs;
+}
+
+turtlelib::Vector2D turtlelib::operator*(const double lhs, turtlelib::Vector2D rhs)
+{
+    rhs.x*=lhs;
+    rhs.y*=lhs;
+    return rhs;
+}
+
+turtlelib::Vector2D turtlelib::operator*(turtlelib::Vector2D lhs, double rhs)
+{
+    lhs.x*=rhs;
+    lhs.y*=rhs;
+    return lhs;
+}
+
+double turtlelib::dot(turtlelib::Vector2D& vec1, turtlelib::Vector2D& vec2)
+{
+    double dp{};
+    dp = vec1.x * vec2.x + vec1.y * vec2.y;
+    return dp;
+}
+
+double turtlelib::magnitude(turtlelib::Vector2D& vec)
+{
+    double mag{};
+    mag = sqrt(vec.x*vec.x + vec.y*vec.y);
+    return mag;
+}
+
+double turtlelib::angle(turtlelib::Vector2D& vec1, turtlelib::Vector2D& vec2){
+    double rad{};
+    const double num{turtlelib::dot(vec1,vec2)};
+    const double den{turtlelib::magnitude(vec1) * turtlelib::magnitude(vec2)};
+    rad = acos(num/den);
+    return rad;
+}
+
+turtlelib::Transform2D::Transform2D() // creates an identity matrix
+    : transf{{1.0,0.0,0.0},{0.0,1.0,0.0},{0.0,0.0,1.0}}
+{
+}
+
+turtlelib::Transform2D::Transform2D(Vector2D trans)
+    : transf{{1.0,0.0,trans.x}
+            ,{0.0,1.0,trans.y}
+            ,{0.0, 0.0, 1.0}}
+{  
+}
+
+turtlelib::Transform2D::Transform2D(double radians)
+    :  transf{{cos(radians),-sin(radians), 0.0}
+             ,{sin(radians),cos(radians), 0.0}
+             ,{0.0, 0.0, 1.0}}
+{
+}
+
+turtlelib::Transform2D::Transform2D(Vector2D trans, double rot)
+    : transf{{cos(rot),-sin(rot), trans.x}
+            ,{sin(rot),cos(rot), trans.y}
+            ,{0.0, 0.0, 1.0}}
+{
+}
+
+turtlelib::Vector2D turtlelib::Transform2D::operator()(turtlelib::Vector2D v) const
+{
+    turtlelib::Vector2D newv{};
+    newv.x = transf[0][0]*v.x + transf[0][1]*v.y + transf[0][2]*1.0;
+    newv.y = transf[1][0]*v.x + transf[1][1]*v.y + transf[1][2]*1.0;
+    return newv;
+}
+
+turtlelib::Transform2D turtlelib::Transform2D::inv() const
+{
+    turtlelib::Transform2D invT{};
+
+    invT.transf[0][0] = transf[0][0];
+    invT.transf[0][1] = transf[0][1]*-1.0;
+    invT.transf[0][2] = -1.0*transf[0][2]*transf[0][0] - transf[1][2]*transf[1][0];
+
+    invT.transf[1][0] = -1.0*transf[1][0];
+    invT.transf[1][1] = transf[1][1];
+    invT.transf[1][2] = -1.0*transf[1][2]*transf[0][0] + transf[0][2]*transf[1][0];
+
+    invT.transf[2][0] = 0.0;
+    invT.transf[2][1] = 0.0;
+    invT.transf[2][2] = 1.0;
+    
+    return invT;
+}
+
+turtlelib::Transform2D & turtlelib::Transform2D::operator*=(const Transform2D & rhs)
+{
+    turtlelib::Transform2D tmp{};
+    tmp = *this; // saving of lhs to temporary struct
+
+    // matrix multiplication
+    tmp.transf[0][0] = transf[0][0]*rhs.transf[0][0] + transf[0][1]*rhs.transf[1][0] + transf[0][2]*rhs.transf[2][0];
+    tmp.transf[0][1] = transf[0][0]*rhs.transf[0][1] + transf[0][1]*rhs.transf[1][1] + transf[0][2]*rhs.transf[2][1];
+    tmp.transf[0][2] = transf[0][0]*rhs.transf[0][2] + transf[0][1]*rhs.transf[1][2] + transf[0][2]*rhs.transf[2][2];
+
+    tmp.transf[1][0] = transf[1][0]*rhs.transf[0][0] + transf[1][1]*rhs.transf[1][0] + transf[1][2]*rhs.transf[2][0];
+    tmp.transf[1][1] = transf[1][0]*rhs.transf[0][1] + transf[1][1]*rhs.transf[1][1] + transf[1][2]*rhs.transf[2][1];
+    tmp.transf[1][2] = transf[1][0]*rhs.transf[0][2] + transf[1][1]*rhs.transf[1][2] + transf[1][2]*rhs.transf[2][2];
+
+    tmp.transf[2][0] = 0.0;
+    tmp.transf[2][1] = 0.0;
+    tmp.transf[2][2] = 1.0;
+
+    *this = tmp;
+    return *this;
+}
+
+turtlelib::Vector2D turtlelib::Transform2D::translation() const
+{
+    turtlelib::Vector2D out_v{};
+    out_v.x = transf[0][2];
+    out_v.y = transf[1][2];
+    return out_v;
+}
+
+double turtlelib::Transform2D::rotation() const
+{
+    double radians{};
+
+    if (transf[0][0]<0 && almost_equal(transf[1][0],0)) // if cos(theta) < 0 and sin(theta) is essentially zero, raises possibility for PI or -PI
+    {
+        double sing{};
+        (transf[1][0]<0) ? sing=-1.0 : sing=1.0; // if sin(theta)<0, PI is positive, and vice versa
+        radians = PI*sing;
+    }
+    else
+    {
+        almost_equal(transf[1][0],0) ? radians=acos(transf[1][1]) : radians=asin(transf[1][0]); // if cos(theta) = 0, use acos(cos(theta)), if not, use asin(sin(theta))
+    }
+    return radians;
+}
+
+turtlelib::Twist2D turtlelib::Transform2D::operator()(turtlelib::Twist2D V) const
+{
+    turtlelib::Twist2D V_new{};
+    double adj[3][3]{{1.0, 0.0, 0.0}
+              ,{transf[1][2], transf[0][0], transf[0][1]}
+              ,{-transf[0][2], transf[1][0], transf[1][1]}};
+    V_new.w = V.w;
+    V_new.v.x = adj[1][0]*V.w + adj[1][1]*V.v.x + adj[1][2]*V.v.y;
+    V_new.v.y = adj[2][0]*V.w + adj[2][1]*V.v.x + adj[2][2]*V.v.y;
+
+    return V_new;
+}
+
+std::ostream & turtlelib::operator<<(std::ostream & os, const turtlelib::Transform2D & tf)
+{
+    os << "deg: " << turtlelib::rad2deg(tf.rotation()) << " x: " << tf.translation().x << " y: " << tf.translation().y;
+    return os;
+}
+
+std::istream & turtlelib::operator>>(std::istream & is, turtlelib::Transform2D & tf)
+{
+    turtlelib::Vector2D vec{};
+    double deg{};
+    int space_pos{0}; // tells you which white space # peek() is at
+    char x{};
+    int num{0}; // set to 1 if a number is entered first, which triggers commands accordingly
+    while (is.peek()!='\n')
+    {
+        x = is.peek();
+        if (x == ' ')
+        {
+            space_pos = space_pos + 1;
+            is.get(x);
+        }
+        else if (std::isdigit(x)||x == '-')
+        {   
+            if(space_pos==0)
+            {
+                num = 1;
+                is >> deg;
+                space_pos = 2;
+            }
+            else if(space_pos==1){is>>deg;}
+            else if(space_pos==3)
+            {
+                is>>vec.x;
+                if(num==1){space_pos = 4;}
+            }
+            else if(space_pos==5){is>>vec.y;}
+        }
+        else
+        {
+            is.get(x);
+        }
+    }
+    // Next two lines are to flush out the \n from the istream
+    x = is.peek();
+    is.get(x);
+    auto radians = turtlelib::deg2rad(deg);
+    turtlelib::Transform2D newtf{vec, radians};
+    tf = newtf;
+    return is;
+}
+
+turtlelib::Transform2D turtlelib::operator*(Transform2D lhs, const Transform2D & rhs)
+{
+    lhs*=rhs;
+    return lhs;
+}
+
+
+turtlelib::Vector2D turtlelib::normalize(Vector2D v)
+{
+    double mag{};
+    mag = sqrt(v.x*v.x + v.y*v.y);
+    v.x /= mag;
+    v.y /= mag;
+    return v;
+}
+
+
+double turtlelib::normalize_angle(double rad)
+{
+    const double quot = rad/(2.0*turtlelib::PI);
+    double rem{}, x{};
+    (quot > 0) ? (rem = quot - floor(quot)) : (rem = quot - ceil(quot));
+    if (rem > 0)
+    {
+        std::cout<<"over 0\n";
+        if (rem > 0.5)
+        {
+            x = rem - 0.5;
+            rad = -1.0*turtlelib::PI + x*2.0*turtlelib::PI;
+        }
+        else if (rem < 0.5)
+        {
+            rad = rem*2.0*turtlelib::PI;
+        }
+        else
+        {
+            rad = turtlelib::PI;
+        }
+        return rad;
+    }
+    else if (rem < 0)
+    {
+        std::cout<<"under 0\n";
+        if (rem > -0.5)
+        {
+            rad = rem*2.0*turtlelib::PI;
+        }
+        else if (x < -0.5)
+        {
+            x = rem + 1.0;
+            rad = x*2.0*turtlelib::PI;
+        }
+        else
+        {
+            rad = turtlelib::PI;
+        }
+        return rad;
+    }
+    else
+    {
+        rad = 0;
+        return rad;
+    }
+}
+
+
+// Twist2D implementations
+
+turtlelib::Twist2D::Twist2D()
+    : w{0}, v{0,0}
+{
+}
+
+turtlelib::Twist2D::Twist2D(double ang_v, Vector2D xy)
+    : w{ang_v}, v{xy}
+{
+}
+
+
+std::ostream & turtlelib::operator<<(std::ostream & os, const Twist2D & tw)
+{
+    os << "[" << tw.w << " " << tw.v.x << " " << tw.v.y << "]";
+    return os;
+}
+
+
+std::istream & turtlelib::operator>>(std::istream & is, Twist2D & tw)
+{
+    turtlelib::Vector2D v{};
+    double w;
+    int space_pos{0};
+    char x{};
+    x = is.peek();
+    is.get(x);
+    while (is.peek()!='\n')
+    {
+        x = is.peek();
+        if (x == ' ')
+        {
+            space_pos += 1;
+            is.get(x);
+        }
+        else if (std::isdigit(x)||x == '-')
+        {   
+            if (space_pos == 0)
+            {
+                is >> w;
+            }
+            else if (space_pos == 1)
+            {
+                is >> v.x;
+            }
+            else if (space_pos == 2)
+            {
+                is >> v.y;
+            }
+        }
+        else
+        {
+            is.get(x);
+        }
+    }
+    
+    x = is.peek();
+    is.get(x);
+    turtlelib::Twist2D tmp{w, v};
+    tw = tmp;
+    return is;
+}
+
+turtlelib::Transform2D turtlelib::integrate_twist(Twist2D& Vb)
+{
+    if (almost_equal(Vb.w,0))
+    {
+        // No t in the expression because we're dealing with one unit time-step
+        const turtlelib::Transform2D Tb_bp{{Vb.v.x, Vb.v.y}, 0.0};
+        return Tb_bp;
+    }
+    else{
+        const turtlelib::Transform2D Ts_b{{Vb.v.y/Vb.w, -1*Vb.v.x/Vb.w}, Vb.w}; // Also equal to Tsp_bp
+        const turtlelib::Transform2D Ts_sp{Vb.w};
+        const turtlelib::Transform2D Tb_bp = Ts_b.inv() * Ts_sp * Ts_b;
+        return Tb_bp;
+    }
+
+}
